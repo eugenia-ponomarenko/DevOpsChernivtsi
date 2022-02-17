de46fff6da107efe12e355a69e5c28e617c111a6
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-02-17 12:49:30 +0200

    Create deploy.sh

---
+++ Geocit/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Remove old project files from tomcat
+sudo rm -rf /opt/tomcat/apache-tomcat-9.0.58/webapps/citizen.war
+sudo rm -rf /opt/tomcat/apache-tomcat-9.0.58/webapps/citizen
+
+#----------------------------------------------------------------------------------------------------
+# Update email credentials
+# email='example@gmail.com'
+# emailpasswd='passwd'
+
+# sed -i "s/ssgeocitizen@gmail.com/$email/g" ~/Geocit134/src/main/resources/application.properties
+# sed -i "s/=softserve/=$emailpasswd/g" ~/Geocit134/src/main/resources/application.properties
+
+#----------------------------------------------------------------------------------------------------
+# Change old IP addresses to new ones
+serverip = 192.168.65.101
+dbip = 192.168.65.102
+
+sed -i "s/http:\/\/192.168.65.101/http:\/\/$serverip/g" ~/Geocit134/src/main/resources/application.properties
+sed -i "s/postgresql:\/\/192.168.65.102/postgresql:\/\/$dbip/g" ~/Geocit134/src/main/resources/application.properties
+
+# Correct path to js folder
+sed -i "s/\/src\/assets/\.\/static/g" ~/Geocit134/src/main/webapp/index.html
+
+sed -i "s/localhost/$serverip/g" ~/Geocit134/src/main/java/com/softserveinc/geocitizen/configuration/MongoConfig.java   
+sed -i "s/localhost/$serverip/g" ~/Geocit134/src/main/webapp/static/js/app.6313e3379203ca68a255.js
+sed -i "s/localhost/$serverip/g" ~/Geocit134/src/main/webapp/static/js/app.6313e3379203ca68a255.js.map
+sed -i "s/localhost/$serverip/g" ~/Geocit134/src/main/webapp/static/js/vendor.9ad8d2b4b9b02bdd427f.js
+sed -i "s/localhost/$serverip/g" ~/Geocit134/src/main/webapp/static/js/vendor.9ad8d2b4b9b02bdd427f.js.map
+
+#-----------------------------------------------------------------------------------------------------
+# build and deploy
+
+mvn install
+sleep 5
+sudo mv target/citizen.war /opt/tomcat/latest/webapps/ 
+sleep 5
+sudo sh /opt/tomcat/latest/bin/startup.sh
