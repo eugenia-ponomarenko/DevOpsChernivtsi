64769a3cde2b350f81be83adf2df2bead6e1b0d1
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-24 20:09:25 +0200

    Update main.tf

--- GeoCitizen/AWS.Terraform+Ansible/Terraform/main.tf
+++ GeoCitizen/AWS.Terraform+Ansible/Terraform/main.tf
@@ -58,8 +58,8 @@ resource "aws_db_instance" "GeoCitizenDB" {
   engine_version         = "12.9"
   instance_class         = "db.t2.micro"
   db_name                = "ss_demo_1"
-  username               = "postgres"
-  password               = "postgres"
+  username               = "..."
+  password               = "..."
   parameter_group_name   = "default.postgres12"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.RDS_SecurityGroup.id]
